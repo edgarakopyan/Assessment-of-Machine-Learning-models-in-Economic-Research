03c3bc1d339cb83c33565e2334cdbd4b99ca3404
Edgar Akopyan <47034548+edgarakopyan@users.noreply.github.com>
2022-03-29 20:56:35 +0200

    Delete Preparation directory

--- Preparation/Folder_Setup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-dir = os.getcwd()
-os.mkdir(dir + "/Results")
-os.mkdir(dir + "/Graphs_and_Tables")
-os.mkdir(dir + "/ACIC")
-for i in range(1, 78):
-    os.mkdir(dir + "/ACIC/" + str(i))
